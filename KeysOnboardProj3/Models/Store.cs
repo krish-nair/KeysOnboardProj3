48bd91aa8ee2b075397d5e331d5725b074f8a594
krish-nair <krish.nair101@gmail.com>
2018-07-05 11:52:16 +1200

    Create Product and Store Tbl

---
+++ KeysOnboardProj3/Models/Store.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace KeysOnboardProj3.Models
+{
+    public class Store
+    {
+        public virtual int Id { get; set; }
+        [Display(Name = "Store Name")]
+        [Required(ErrorMessage = "Store name is required")]
+        [RegularExpression(@"^[a-zA-Z0-9'' '] + $", ErrorMessage = "Special charecters not allowed")]
+        public virtual string Name { get; set; }
+        [Display(Name = "Store Address")]
+        public virtual String Address { get; set; }
+    }
+}
