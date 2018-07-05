8ea6eabc66c085585064f8923b7a4cb12a09c6e7
krish-nair <krish.nair101@gmail.com>
2018-07-05 12:22:52 +1200

    Add Productr Controller

--- KeysOnboardProj3/Models/KnockoutDbContext.cs
+++ KeysOnboardProj3/Models/KnockoutDbContext.cs
@@ -9,5 +9,7 @@ namespace KeysOnboardProj3.Models
     public class KnockoutDbContext : DbContext
     {
         public System.Data.Entity.DbSet<KeysOnboardProj3.Models.Customer> Customers { get; set; }
+
+        public System.Data.Entity.DbSet<KeysOnboardProj3.Models.Product> Products { get; set; }
     }
 }
